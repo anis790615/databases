52422e51243a31a257f6604c90cf110160b1b6b6
Anis Alkomem <anis.alkumaim@gmail.com>
2020-06-20 13:22:40 +0200

    Added asyncronous code into exercises 1 and 2

--- Week2/homework/exercises/1-Keys.js
+++ Week2/homework/exercises/1-Keys.js
@@ -1,31 +1,44 @@
+const util = require("util");
 const mysql = require("mysql");
 
-const db = mysql.createConnection({
+const CONNECTION_CONFIG = {
   host: "localhost",
   user: "hyfuser",
   password: "hyfpassword",
   database: "userdb",
   multipleStatements: true,
-});
-db.connect();
-db.query(
-  `CREATE TABLE IF NOT EXISTS Authors(author_no INT AUTO_INCREMENT PRIMARY KEY,
-  author_name VARCHAR(50),
-  university VARCHAR(250), 
-  date_of_birth DATE,
-  h_index INT, gender ENUM('m','f','o'));`,
-  (error, results, fields) => {
-    if (error) throw error;
-    console.log("Table created...");
+};
+function dropTable(tableName) {
+  return `DROP TABLE IF EXISTS ${tableName};`;
+}
+const CREATE_AUTHORS_TABLE = `
+    CREATE TABLE IF NOT EXISTS Authors
+    (author_no INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(50),
+    university VARCHAR(250), 
+    date_of_birth DATE,
+    h_index INT, gender ENUM('m','f','o'));
+    `;
+const UPDATE_AUTHORS_TABLE = `
+    ALTER TABLE Authors
+    ADD COLUMN collaborator INT,
+    ADD CONSTRAINT fk_author FOREIGN KEY(collaborator) 
+    REFERENCES Authors(author_no);
+    `;
+async function createAndUpdateTables() {
+  const connection = mysql.createConnection(CONNECTION_CONFIG);
+  const runQuery = util.promisify(connection.query.bind(connection));
+  try {
+    await runQuery(dropTable("Authors"));
+    console.log("Table Authors dropped ...");
+    await runQuery(CREATE_AUTHORS_TABLE);
+    console.log("Table Authors Created ...");
+    await runQuery(UPDATE_AUTHORS_TABLE);
+    console.log("Table Authors Updated ...");
+    connection.end();
+  } catch (err) {
+    console.log(err.message);
+    connection.end();
   }
-);
-db.query(
-  `ALTER TABLE Authors
-   ADD COLUMN collaborator INT,
-   ADD CONSTRAINT fk_author FOREIGN KEY(collaborator) REFERENCES Authors(author_no); `,
-  (error, results, fields) => {
-    if (error) throw error;
-    console.log("Column Added...");
-  }
-);
-db.end();
+}
+createAndUpdateTables();
--- Week2/homework/exercises/2-relationship.js
+++ Week2/homework/exercises/2-relationship.js
@@ -1,39 +1,107 @@
+const util = require("util");
 const mysql = require("mysql");
+const fs = require("fs");
+const path = require("path");
 
-const db = mysql.createConnection({
+const CONNECTION_CONFIG = {
   host: "localhost",
   user: "hyfuser",
   password: "hyfpassword",
   database: "userdb",
   multipleStatements: true,
-});
-db.connect();
-// Creating the research papers table with a primay key of paper_id
-db.query(
-  `DROP TABLE IF EXISTS Research_Papers;
+};
+function dropTable(tableName) {
+  return `DROP TABLE IF EXISTS ${tableName};`;
+}
+const CREATE_RESEARCH_PAPERS_TABLE = `
   CREATE TABLE IF NOT EXISTS Research_Papers
   (paper_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   paper_title VARCHAR(100),
   conference VARCHAR(50), 
-  publish_date DATE);`,
-  (error, results, fields) => {
-    if (error) throw error;
-    console.log("Research Papers Table created...");
-  }
-);
-// As the relation ship between Authors and paper is many-to-many a authors_papers table is created
-db.query(
-  `DROP TABLE IF EXISTS Author_Papers;
+  publish_date DATE);`;
+const CREATE_AUTHORS_PAPERS_TABLE = `
   CREATE TABLE IF NOT EXISTS Author_Papers
   (paper_id INT NOT NULL,
   author_id INT NOT NULL,
   PRIMARY KEY(paper_id, author_id),
-  FOREIGN KEY (paper_id) REFERENCES Research_Papers(paper_id) ON DELETE CASCADE,
-  FOREIGN KEY (author_id) REFERENCES Authors(author_no) ON DELETE CASCADE);`,
-  (error, results, fields) => {
-    if (error) throw error;
-    console.log("Author-Paper Table created...");
-  }
-);
+  FOREIGN KEY (paper_id) 
+  REFERENCES Research_Papers(paper_id) ON DELETE CASCADE,
+  FOREIGN KEY (author_id) 
+  REFERENCES Authors(author_no) ON DELETE CASCADE);`;
+// Because data that references unexisting elemnts can't be entered two queries are created, first to drop the foreign key of the collborator column, and then to set again after the data was entered. Another option might be to enter the data into the collborator column separately
+const DROP_FOREIGN_KEY = `
+  ALTER TABLE Authors 
+  DROP FOREIGN KEY fk_author;
+  `;
+const ADD_FOREIGN_KEY = `
+  ALTER TABLE Authors
+  ADD CONSTRAINT fk_author FOREIGN KEY(collaborator) 
+  REFERENCES Authors(author_no);
+`;
 
-db.end();
+async function createAndFillTables() {
+  const connection = mysql.createConnection(CONNECTION_CONFIG);
+  const readFile = util.promisify(fs.readFile);
+  const runQuery = util.promisify(connection.query.bind(connection));
+  try {
+    // Creating the table research papers and the link table author_research papers, dropping them first at the beginning of each run for testing.
+    await runQuery(dropTable("Author_Papers"));
+    console.log("Table Author_Papers dropped ...");
+    await runQuery(dropTable("Research_Papers"));
+    console.log("Table Research_Papers dropped ...");
+    await runQuery(CREATE_RESEARCH_PAPERS_TABLE);
+    console.log("Table Research_Papers Created ...");
+    await runQuery(CREATE_AUTHORS_PAPERS_TABLE);
+    console.log("Table Author_Papers Created ...");
+    // Fetching data to be inserted from files, each data fetched separately for authors, researc papers, and author_papers
+    const authorsData = await readFile(
+      path.join(__dirname, "/authors.json"),
+      "utf8"
+    );
+    const authors = JSON.parse(authorsData);
+    const papersData = await readFile(
+      path.join(__dirname, "/research-papers.json"),
+      "utf8"
+    );
+    const papers = JSON.parse(papersData);
+    const authorPapersData = await readFile(
+      path.join(__dirname, "/author-papers.json"),
+      "utf8"
+    );
+    const authorPapers = JSON.parse(authorPapersData);
+
+    // Insert data into Authors table
+    // First foreign key is dropped
+    await runQuery(DROP_FOREIGN_KEY);
+    console.log("Foreign key fk_author dropped");
+    // Insert Data
+    const insertAuthorDataPromises = authors.map((author) =>
+      runQuery("INSERT INTO Authors SET ?", author)
+    );
+    await Promise.all(insertAuthorDataPromises);
+    console.log("Authors information has been inserted... ");
+
+    // Add foreign key
+    await runQuery(ADD_FOREIGN_KEY);
+    console.log("Foreign key fk_author added");
+
+    // Insert data into Research Papers table
+    const insertPaperDataPromises = papers.map((paper) =>
+      runQuery("INSERT INTO Research_Papers SET ?", paper)
+    );
+    await Promise.all(insertPaperDataPromises);
+    console.log("Research paper information has been inserted... ");
+    // Insert data into Author_Papers table
+    const insertAuthorPaperDataPromises = authorPapers.map((authorPaper) =>
+      runQuery("INSERT INTO Author_Papers SET ?", authorPaper)
+    );
+    await Promise.all(insertAuthorPaperDataPromises);
+    console.log("Author-Paper Relation information has been inserted... ");
+
+    connection.end();
+  } catch (err) {
+    console.log(err.message);
+    connection.end();
+  }
+}
+createAndFillTables();
